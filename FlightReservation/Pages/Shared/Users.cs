12bb96117f7c2f95c636435d8b370f6790811351
consumingsouls <98334120+consumingsouls@users.noreply.github.com>
2023-02-15 17:24:31 -0700

    Flight Reservation System
    code for air time
    depart time
    ticket number
    seats available
    status
    flight number
    Cancellation Date

---
+++ FlightReservation/Pages/Shared/Users.cs
@@ -0,0 +1,9 @@
+﻿namespace FlightReservation.Pages.Shared
+{
+    public class Users
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
+    }
+}
